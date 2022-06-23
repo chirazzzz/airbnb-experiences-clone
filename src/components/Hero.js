0f530df457d35512414ebcfbd8591fae863a8404
chirazzzz <chiragmehta.uk@gmail.com>
2022-06-23 16:12:27 +0100

    minor changes to hero styling

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,14 +3,12 @@ import heroImg from "../images/photo-grid.png";
 
 function Hero() {
   return (
-    <main className="hero">
+    <section className="hero">
       <img src={heroImg} alt="Airbnb logo" className="hero--img" />
-      <div className="hero--text">
         <h1 className="hero--header">Online Experiences</h1>
         <p className="hero--desc">Join unique interactive activities 
         led by one-of-a-kind hosts—all without leaving home.</p>
-      </div>
-    </main>
+    </section>
   );
 }
 
